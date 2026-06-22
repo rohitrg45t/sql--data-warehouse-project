2c7068ac96d361f70721dd8743df20ec1706d4f6
rohitrg45t <rohitgore8844@gmail.com>
2026-06-22 14:01:18 +0530

    Create init_database.sql
    add init_database "datawarehouse"

---
+++ scripts/init_database.sql
@@ -0,0 +1,8 @@
+-- create  "datawarehouse" database
+CREATE DATABASE DataWarehouse;
+SHOW DATABASES;
+USE DataWarehouse;
+-- create schemas
+CREATE schema bronze;
+CREATE schema silver;
+CREATE schema gold;
