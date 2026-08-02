f31d5d51a3059a3dd343e1ef1918ea8b33118b3f
rohitrg45t <rohitgore8844@gmail.com>
2026-08-02 10:37:46 +0530

    Update and rename ddl to ddl_silver.sql
    add purpose of script

--- scripts/dw_silver/ddl_silver.sql
+++ scripts/dw_silver/ddl_silver.sql
@@ -1,3 +1,16 @@
+/*
+************************************************************
+DDL Script: Create Silver Tables
+************************************************************
+Script Purpose:
+    This script creates tables in the 'silver' schema ,dropping existing tables
+    if they already exist.
+    Runs this script to re-define the DDL structure of 'bronze' Tables
+*************************************************************
+*/
+
+
+
 USE dw_silver;
 DROP TABLE IF EXISTS dw_silver.crm_cust_info;
 CREATE TABLE dw_silver.crm_cust_info(
