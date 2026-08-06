474641b30e7fef3e15ce6de475525e82932269b3
rohitrg45t <rohitgore8844@gmail.com>
2026-08-06 23:03:31 +0530

    Update ddl_gold.sql
    ADD objects(view)

--- scripts/dw_gold/ddl_gold.sql
+++ scripts/dw_gold/ddl_gold.sql
@@ -1,4 +1,33 @@
- CREATE VIEW dw_gold.dim_products AS 
+-- ===============================================================
+-- create object(view) of gold: dw_gold.dim_customers
+-- ===============================================================
+
+    CREATE OR REPLACE VIEW dw_gold.dim_customers AS 
+    SELECT 
+    ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key,
+    ci.cst_id AS customer_id,
+    ci.cst_key AS customer_number,
+    ci.cst_firstname AS first_name,
+    ci.cst_lastname AS last_name,
+	la.cntry AS country,
+    ci.cst_marital_status AS marital_status,
+    CASE WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr -- CRM is the master for gender info.
+            ELSE COALESCE(ca.gen,'n/a') -- 'COALESCE()' works as a chain if ca.gen not NULL -> ca.gen(Female or Male), if ca.gen NULL -> 'n/a'
+		END AS gender,
+	ca.bdate AS birthdate,
+    ci.cst_create_date AS create_date
+    FROM dw_silver.crm_cust_info AS ci
+    LEFT JOIN dw_silver.erp_cust_az12 AS ca
+    ON ci.cst_key= ca.cid
+    LEFT JOIN dw_silver.erp_loc_a101 AS la
+    ON ci.cst_key = la.cid;  
+
+
+-- ================================================================
+-- create object(view) of gold: dw_gold.dim_products
+-- ================================================================
+
+CREATE  OR REPLACE VIEW dw_gold.dim_products AS 
     SELECT
     ROW_NUMBER() OVER (ORDER BY pn.prd_id,pn.prd_start_dt) AS product_key,
     pn.prd_id AS product_id,
@@ -16,3 +45,24 @@
     LEFT JOIN dw_silver.erp_px_cat_g1v2 pc
     ON pn.cat_id = pc.id
     WHERE prd_end_dt IS NULL;
+
+-- ================================================================
+-- create object(view) of gold: dw_gold.fact_sales
+-- ================================================================
+CREATE OR REPLACE  VIEW dw_gold.fact_sales AS
+SELECT
+sd.sls_ord_num AS order_number,
+pr.product_key,
+cu.customer_key,
+sd.sls_order_dt AS order_date,
+sd.sls_ship_dt AS shipping_date,
+sd.sls_due_dt AS due_date,
+sd.sls_sales AS sales_amount,
+sd.sls_quantity AS quantity,
+sd.sls_price AS price
+FROM dw_silver.crm_sales_details sd
+LEFT JOIN dw_gold.dim_products pr
+ON sd.sls_prd_key = pr.product_number
+LEFT JOIN dw_gold.dim_customers cu
+ON sd.sls_cust_id = cu.customer_id;
+
